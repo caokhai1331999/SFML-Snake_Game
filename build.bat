64df032aff9d2ad832b5951812a083046047cf4c
Cao Khai <goku1331999@gmail.com>
2023-04-18 13:02:28 +0700

    keep removing bat files

--- build.bat
+++
@@ -1,2 +0,0 @@
-g++ -fexceptions -DSFML_STATIC -I"C:/Users/klove/Downloads/SFML/SFML-2.5.1 for MINGW/SFML-2.5.1/include" -c  *.cpp  *.h
-g++ *.o -o main -L"C:/Users/klove/Downloads/SFML/SFML-2.5.1 for MINGW/SFML-2.5.1/lib" -lsfml-graphics -lsfml-window -lsfml-system
